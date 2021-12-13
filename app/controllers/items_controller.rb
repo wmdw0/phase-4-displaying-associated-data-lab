d7e3a418b60e19e2cf4fd2cb553f2801f53b35c7
William Walsh <wmdwalsh0@gmail.com>
2021-12-13 01:03:28 -0500

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        items = Item.all.order(name: :desc)
+        render json: items, include: :user
+      end
+
+#       private
+
+#   def render_not_found_response
+#     render json: { error: "Dog house not found" }, status: :not_found
+#   end
 end
